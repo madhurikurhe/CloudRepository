84d70cccc39b9cedac6a81ba32441adc55039cb4
madhuriunde <madhuriunde123@gmail.com>
2018-03-07 16:34:02 +0530

    Boolean Test Program

---
+++ CoreJava/Chapter1/src/com/headfirst/chapter1/BooleanTest.java
@@ -0,0 +1,28 @@
+
+package com.headfirst.chapter1;
+import java.util.Scanner;
+public class BooleanTest {
+
+	public static void main(String args[]) {
+
+		 Scanner s = new Scanner(System.in);
+	      System.out.print("Enter first number: ");
+	      int firstNumber = s.nextInt();
+	      System.out.print("Enter second number: ");
+	      int secondNumber = s.nextInt();
+	      
+	      if(firstNumber>secondNumber)
+	      {
+	    	  System.out.print("First number is greater than second");
+	      }
+	      else if(secondNumber>firstNumber)
+	      {
+	    	  System.out.print("Second number is greater than first");
+	      }
+	      else
+	    	  
+	    	  
+	      
+	      System.out.println("Two numbers are equal ");
+	}
+}
