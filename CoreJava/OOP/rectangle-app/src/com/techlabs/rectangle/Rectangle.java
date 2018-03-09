056a2a2f03722acbcbdef530c1c1feb7b14c70f8
madhuriunde <madhuriunde123@gmail.com>
2018-03-09 16:19:53 +0530

    Area of rectangle with constructor

--- CoreJava/OOP/rectangle-app/src/com/techlabs/rectangle/Rectangle.java
+++ CoreJava/OOP/rectangle-app/src/com/techlabs/rectangle/Rectangle.java
@@ -1,7 +1,15 @@
 package com.techlabs.rectangle;
 
 public class Rectangle {
-	public int calculateArea(int height, int width) {
+	public static int height;
+	public static int width;
+
+	public Rectangle(int height1, int width1) {
+		height = height1;
+		width = width1;
+	}
+
+	public static int calculateArea() {
 		int area = height * width;
 		return area;
 
--- CoreJava/OOP/rectangle-app/src/com/techlabs/rectangle/test/RectangleTest.java
+++ CoreJava/OOP/rectangle-app/src/com/techlabs/rectangle/test/RectangleTest.java
@@ -3,13 +3,13 @@ package com.techlabs.rectangle.test;
 import com.techlabs.rectangle.Rectangle;
 
 public class RectangleTest {
-	public static void main(String[] args) {
-		Rectangle test = new Rectangle();
 
-		System.out.println("Area of Rectangle is: "
-				+ test.calculateArea(10, 10));
-		System.out.println("Area of Rectangle is: "
-				+ test.calculateArea(20, 10));
+	public static void main(String[] args) {
+		Rectangle test = new Rectangle(10, 10);
+		System.out.println("Area of Rectangle is: " + test.calculateArea());
+
+		Rectangle test1 = new Rectangle(20, 20);
+		System.out.println("Area of Rectangle is: " + test1.calculateArea());
 
 	}
 }
