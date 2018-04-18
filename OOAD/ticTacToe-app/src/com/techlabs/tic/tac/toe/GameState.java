820a5a0f9f38bf02521854101d1d0168f2eaf023
madhuriunde <madhuriunde123@gmail.com>
2018-04-18 14:57:49 +0530

    Tic tac toe

---
+++ OOAD/ticTacToe-app/src/com/techlabs/tic/tac/toe/GameState.java
@@ -0,0 +1,5 @@
+package com.techlabs.tic.tac.toe;
+
+public enum GameState {
+	PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
---
+++ OOAD/ticTacToe-app/src/com/techlabs/tic/tac/toe/Seed.java
@@ -0,0 +1,5 @@
+package com.techlabs.tic.tac.toe;
+
+public enum Seed {
+	EMPTY, CROSS, NOUGHT
+}
