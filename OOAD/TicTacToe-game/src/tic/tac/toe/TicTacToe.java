3e1ed9d7614e1b18bd4f0e17d9bb7e3dbc510d23
madhurikurhe <madhuriunde123@gmail.com>
2018-05-24 17:01:25 +0530

    Tic Tac Toe Latest version

--- OOAD/TicTacToe-game/src/tic/tac/toe/TicTacToe.java
+++ OOAD/TicTacToe-game/src/tic/tac/toe/TicTacToe.java
@@ -38,31 +38,30 @@ public class TicTacToe extends JFrame implements ActionListener {
 		setSize(500, 500);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		panel.setLayout(new GridLayout(4, 4));
+		panel.setLayout(new GridLayout(3, 3));
 
-		JTextField statusBar1 = new JTextField("Madhu's Turn");
+		JTextField statusBar1 = new JTextField("Madhu's Turn(X)");
 		statusBar1.setEditable(false);
 		add(statusBar1, BorderLayout.SOUTH);
 
 		for (int i = 0; i < 9; i++) {
 			buttons[i] = new JButton();
-
-			x = new ImageIcon(this.getClass().getResource("x3.png"));
+			x = new ImageIcon(this.getClass().getResource("x1.png"));
 			o = new ImageIcon(this.getClass().getResource("o1.png"));
 			buttons[i].addActionListener(this);
 			buttons[i].putClientProperty("id", Integer.valueOf(i));
 			panel.add(buttons[i]);
 		}
 
-		panel.add(reset);
-		reset.setText("Reset");
-		reset.addActionListener(this);
-
 		add(panel);
 		setVisible(true);
 		game.initGame();
 		player[0] = new Player("Madhu", Mark.CROSS);
 		player[1] = new Player("Trisha", Mark.NOUGHT);
+
+	}
+
+	private void displayDetails() {
 
 		if (game.getCurrentState() != GameState.DRAW) {
 			if (game.getCurrentPlayer() == Mark.CROSS)
@@ -81,27 +80,6 @@ public class TicTacToe extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
-		if (game.getCurrentState() == GameState.PLAYING) {
-			if (game.getCurrentPlayer() == Mark.CROSS) {
-
-				JTextField statusBar = new JTextField("Madhu's Turn");
-				statusBar.setEditable(false);
-				add(statusBar, BorderLayout.SOUTH);
-
-			} else {
-				JTextField statusBar = new JTextField("Trisha's Turn");
-				statusBar.setEditable(false);
-				add(statusBar, BorderLayout.SOUTH);
-			}
-
-			try {
-
-			} catch (Exception ex) {
-				System.out.println(ex.getMessage());
-			}
-
-		}
 
 		JButton b = (JButton) e.getSource();
 		int cellLocation = 0;
@@ -126,8 +104,23 @@ public class TicTacToe extends JFrame implements ActionListener {
 		}
 		try {
 			game.play(cellLocation);
+			if (game.getCurrentState() != GameState.PLAYING)
+				displayDetails();
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
+
+		if (game.getCurrentPlayer() == Mark.CROSS) {
+
+			JTextField statusBar = new JTextField("Madhu's Turn(X)");
+			statusBar.setEditable(false);
+			add(statusBar, BorderLayout.SOUTH);
+
+		} else {
+			JTextField statusBar = new JTextField("Trisha's Turn(O)");
+			statusBar.setEditable(false);
+			add(statusBar, BorderLayout.SOUTH);
+		}
+
 	}
 }
